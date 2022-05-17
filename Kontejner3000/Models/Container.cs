3245b9a076a3fbd410d68651863bd4bb49a18b09
Eidamiino <adam.pala199@gmail.com>
2022-05-17 23:52:13 +0200

    basical functionality added (took ages because of me trying to get used to english keyboard)

--- Kontejner3000/Models/Container.cs
+++ Kontejner3000/Models/Container.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Kontejner3000
 {
@@ -9,5 +10,36 @@ namespace Kontejner3000
 			ContainerId = Guid.NewGuid();
 		}
 		public Guid ContainerId{get;protected set;}
+
+		public static void AddBoxesUntilFull(Container container,List<Box> boxes)
+		{
+			for (int i = 0; i < boxes.Count; i++)
+			{
+				container.PrintFreeSpace();
+				if (container.IsBoxBigger(boxes[i]))
+				{
+					Console.WriteLine($"The container is full!");
+					break;
+				}
+
+				container.AddBox(boxes[i]);
+			}
+		}
+
+		public void PrintFreeSpace()
+		{
+			Console.WriteLine($"Current free space in container: {this.Volume}");
+		}
+		public void AddBox(Box box)
+		{
+			this.Volume-=box.Volume;
+		}
+
+		public bool IsBoxBigger(Box box)
+		{
+			if (box.Volume > this.Volume)
+				return true;
+			return false;
+		}
 	}
 }
--- Kontejner3000/Program.cs
+++ Kontejner3000/Program.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Kontejner3000
 {
 	internal class Program
 	{
+		public static List<Box> Boxes = new List<Box>();
 		static void Main(string[] args)
 		{
-			Box box1=Helpers.GenerateRandomBox();
-			Console.WriteLine(box1);
+			for (int hroch = 0; hroch < 20; hroch++)
+			{
+				var box = Helpers.GenerateRandomBox();
+				Boxes.Add(box);
+				Console.WriteLine(box);
+			}
+			Container container = new Container(1000,10,100,100);
+
+			Container.AddBoxesUntilFull(container, Boxes);
 		}
 	}
 }
